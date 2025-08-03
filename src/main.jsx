f46f0b40b78ce8b1a83bf7872284866b1642b35a
junglesub <junglesubmarine@gmail.com>
2025-08-03 17:42:42 +0900

    feat: use hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,56 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { redirectHandler } from "./pages/RedirectPage.jsx";
 import Privacy from "./pages/Privacy.jsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "/privacy",
-      element: <Privacy />,
-    },
-    {
-      path: "/form",
-      element: redirectHandler(
-        "https://docs.google.com/forms/d/e/1FAIpQLScoN19fAklBenQlIPES3KYiInuK26iAkyHka6iPTnPdFRKwlQ/viewform?usp=sf_link"
-      ),
-    },
-    {
-      path: "/ios",
-      element: redirectHandler(
-        "https://apps.apple.com/app/%EB%B9%A0%EB%A5%B8-%EC%83%81%EC%83%81%EB%9E%A9/id6648756420"
-      ),
-    },
-    {
-      path: "/android",
-      element: redirectHandler(
-        "https://docs.google.com/forms/d/e/1FAIpQLSfJLIpDF_AaLXocA-saLB3ndBU5Yjjrrts47SH2SVItbPUvAw/viewform?usp=sf_link"
-      ),
-    },
-    {
-      path: "*",
-      element: <Navigate replace to="/" />,
-    },
-  ],
+// BrowserRouter 경로를 HashRouter 경로로 리디렉트
+function redirectToHashIfNeeded() {
+  const { pathname, search, hash } = window.location;
+  // 이미 해시 라우팅이면 무시
+  if (hash.startsWith("#/")) return;
+  // root만 예외로 두고 싶으면 아래 조건 추가
+  if (pathname === "/" && !search) return;
+  const newHash = `#${pathname}${search}`;
+  window.location.replace(newHash);
+}
+
+redirectToHashIfNeeded();
+
+const router = createHashRouter([
   {
-    basename: import.meta.env.PUBLIC_URL,
-  }
-);
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/privacy",
+    element: <Privacy />,
+  },
+  {
+    path: "/form",
+    element: redirectHandler(
+      "https://docs.google.com/forms/d/e/1FAIpQLScoN19fAklBenQlIPES3KYiInuK26iAkyHka6iPTnPdFRKwlQ/viewform?usp=sf_link"
+    ),
+  },
+  {
+    path: "/ios",
+    element: redirectHandler(
+      "https://apps.apple.com/app/%EB%B9%A0%EB%A5%B8-%EC%83%81%EC%83%81%EB%9E%A9/id6648756420"
+    ),
+  },
+  {
+    path: "/android",
+    element: redirectHandler(
+      "https://docs.google.com/forms/d/e/1FAIpQLSfJLIpDF_AaLXocA-saLB3ndBU5Yjjrrts47SH2SVItbPUvAw/viewform?usp=sf_link"
+    ),
+  },
+  {
+    path: "*",
+    element: <App />, // NotFound 처리: 홈으로
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
